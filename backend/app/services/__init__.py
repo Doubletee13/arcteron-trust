2fd8e757fe43c125fe9d8da13d262eae69f5c58c
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2026-05-17 23:43:42 +0100

    feat: register all services in package

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,20 @@
+from app.services.auth_service import (
+    register_step1,
+    register_step2,
+    register_step3,
+    login_user
+)
+
+from app.services.auth_service import (
+    register_step1,
+    register_step2,
+    register_step3,
+    login_user
+)
+from app.services.email_service import (
+    send_login_alert,
+    send_password_reset_email,
+    send_pin_reset_email,
+    send_transfer_sent_email,
+    send_transfer_received_email
+)
