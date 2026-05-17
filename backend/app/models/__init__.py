6b13a204c845dee9345969900bbe8875b4cdd49c
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2026-05-17 23:40:13 +0100

    feat: register all models in package

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.user import User, UserRole, UserStatus
+from app.models.account import Account
+from app.models.transaction import Transaction, TransactionType, TransactionStatus
+from app.models.notification import Notification, NotificationType
+from app.models.audit_log import AuditLog
+from app.models.code import TransactionCode, CodeType
